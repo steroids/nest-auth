34b41027f50ebf10d6ee64596b1b5551cb776cd4
Vladislav Koktashev <vladislav54415@gmail.com>
2024-08-16 15:14:45 +0300

    Update AuthUpdateUserOwnPasswordUseCaseDto.ts

--- src/usecases/updatePassword/dtos/AuthUpdateUserOwnPasswordUseCaseDto.ts
+++ src/usecases/updatePassword/dtos/AuthUpdateUserOwnPasswordUseCaseDto.ts
@@ -1,6 +1,6 @@
-import { StringField } from '@steroidsjs/nest/infrastructure/decorators/fields';
-import { Validator } from '@steroidsjs/nest/usecases/validators';
-import { PasswordValidator } from '../../../infrastructure/validators/PasswordValidator';
+import {StringField} from '@steroidsjs/nest/infrastructure/decorators/fields';
+import {Validator} from '@steroidsjs/nest/usecases/validators';
+import {PasswordValidator} from '../../../infrastructure/validators/PasswordValidator';
 
 export class AuthUpdateUserOwnPasswordUseCaseDto {
     @StringField({
