07fdec4f02d2fe0fce6739a7365d0954eff5ffd7
Danil <danil_ustinov@mail.ru>
2024-11-25 13:43:41 +0700

    remove IDeepPartial from tables

--- src/infrastructure/tables/AuthRoleTable.ts
+++ src/infrastructure/tables/AuthRoleTable.ts
@@ -1,6 +1,5 @@
-import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
 import {TypeOrmTableFromModel} from '@steroidsjs/nest/infrastructure/decorators/typeorm/TypeOrmTableFromModel';
 import {AuthRoleModel} from '../../domain/models/AuthRoleModel';
 
 @TypeOrmTableFromModel(AuthRoleModel, 'auth_role')
-export class AuthRoleTable implements IDeepPartial<AuthRoleModel> {}
+export class AuthRoleTable extends AuthRoleModel {}
