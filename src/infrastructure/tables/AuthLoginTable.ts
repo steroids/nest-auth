c3f4ef2c94e7ac1eb77a83c62aba4f7476026028
Vladislav Koktashev <vladislav54415@gmail.com>
2025-05-12 16:59:21 +0300

    0.2.0 version update

--- src/infrastructure/tables/AuthLoginTable.ts
+++ src/infrastructure/tables/AuthLoginTable.ts
@@ -1,6 +1,6 @@
-import {TableFromModel} from '@steroidsjs/nest/infrastructure/decorators/TableFromModel';
 import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
+import {TypeOrmTableFromModel} from '@steroidsjs/nest/infrastructure/decorators/typeorm/TypeOrmTableFromModel';
 import {AuthLoginModel} from '../../domain/models/AuthLoginModel';
 
-@TableFromModel(AuthLoginModel, 'auth_login')
+@TypeOrmTableFromModel(AuthLoginModel, 'auth_login')
 export class AuthLoginTable implements IDeepPartial<AuthLoginModel> {}
