0a1d66506cb046809d33c91d1162eb9b66267c3a
kkuchin <kkuchin@kozhin.dev>
2026-02-25 20:11:02 +0700

    #209 NotFoundException on user not found

--- src/usecases/sendAuthenticationCodeUseCase/SendAuthenticationCodeUseCase.ts
+++ src/usecases/sendAuthenticationCodeUseCase/SendAuthenticationCodeUseCase.ts
@@ -1,6 +1,6 @@
 import {IUserService} from '@steroidsjs/nest-modules/user/services/IUserService';
 import {ContextDto} from '@steroidsjs/nest/usecases/dtos/ContextDto';
-import {Inject, Injectable} from '@nestjs/common';
+import {Inject, Injectable, NotFoundException} from '@nestjs/common';
 import {AuthConfirmService} from '../../domain/services/AuthConfirmService';
 import {AuthConfirmSendCodeDto} from '../../domain/dtos/AuthConfirmSendCodeDto';
 import {AuthConfirmModel} from '../../domain/models/AuthConfirmModel';
@@ -23,7 +23,7 @@ export class SendAuthenticationCodeUseCase implements ISendAuthenticationCodeUse
         const user = await this.userService.findByLogin(dto.phone);
 
         if (!user) {
-            throw new Error('Пользователь не найден');
+            throw new NotFoundException('Пользователь не найден');
         }
 
         const sendCodeDto: AuthConfirmSendCodeDto = {
