32a546afbfcf320f9ffd5417f686e74f6e6112c9
RaayNoff <kraxtv471365@gmail.com>
2026-08-14 12:28:15 +0700

    feat: added purpose consts

--- src/domain/constants/index.ts
+++ src/domain/constants/index.ts
@@ -1,2 +1,4 @@
 export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken';
 export const ACCESS_TOKEN_COOKIE_NAME = 'accessToken';
+export const AUTH_CONFIRM_DEFAULT_PURPOSE = 'default';
+export const AUTH_CONFIRM_LOGIN_PURPOSE = 'login';
